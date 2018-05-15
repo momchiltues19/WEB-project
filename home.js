6aec188dee55514209d2a23ebf2c223916589fbd
momchil <momchiltues19>
2018-05-16 00:37:37 +0300

    Fixed some small issues

--- home.js
+++ home.js
@@ -2,3 +2,13 @@ window.onload = function()
 {
 
 };
+
+function bigImg(x) {
+    x.style.height = "512px";
+    x.style.width = "512px";
+}
+
+function normalImg(x) {
+    x.style.height = "256px";
+    x.style.width = "256px";
+}
