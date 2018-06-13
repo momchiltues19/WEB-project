b71633e96f15c84a23e08a25bfd1aa67edc09307
stefan50 <ste.angelov.tues19@gmail.com>
2018-06-13 17:24:53 +0300

    Delete unneeded file

--- index.js
+++
@@ -1,7 +0,0 @@
-function upgradeImage(object)
-{
-    var id = object.id;
-    var target = "div_" + id.substring(4);
-    
-    document.getElementById(target).style.backgroundImage = "url(" + object.src + ")";
-}
